cbe2e333aca2938d23710c38754beec12acbba62
namestesensei <christopherwright435@gmail.com>
2024-05-20 22:12:15 +0000

    4.9

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Prints a string using puts
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
-    char *message = "Programming is like building a multilingual puzzle";
-
-    /* Print the message */
-    puts(message);
+    /* Print the message using puts */
+    puts("Programming is like building a multilingual puzzle");
 
     return (0);
 }
+
