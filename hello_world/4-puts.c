31783093cb3c67e337ecb077f3a227c622f85af5
namestesensei <christopherwright435@gmail.com>
2024-05-20 08:36:14 +0000

    4.7

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,11 @@
-/* File: 4-puts.c */
-/* Directory: hello_world */
-/* Repository: atlas-low_level_programming */
-
 #include <stdio.h>
 
-/**
- * main - Prints a message using puts
- *
- * Return: 0 on success
- */
 int main(void)
 {
-    /* Print the message */
-    puts("Programming is like building a multilingual puzzle.");
+    char *message = "Programming is like building a multilingual puzzle";
 
-    /* Return 0 to indicate success */
-    return 0;
+    /* Print the message */
+    puts(message);
+
+    return (0);
 }
