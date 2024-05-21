cdc12b18bd2ef5abc8f2a19655c56c048144e4ab
namestesensei <christopherwright435@gmail.com>
2024-05-21 04:16:01 +0000

    8.0

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+
+    for (num = 48; num < 58; num++)
+    {
+        putchar(num);
+    }
+
+    for (num = 97; num < 103; num++)
+    {
+        putchar(num);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
