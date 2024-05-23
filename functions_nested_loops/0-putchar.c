b207c1d551abb10bd898b1db45af36a63ce67f5a
namestesensei <christopherwright435@gmail.com>
2024-05-23 05:51:35 +0000

    0.2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,21 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
 int _putchar(char c);
 
+/**
+ * main - Entry point
+ *
+ * This function prints "_putchar", followed by a new line.
+ * It doesn't take any arguments and returns 0 when done.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	_putchar('_');
