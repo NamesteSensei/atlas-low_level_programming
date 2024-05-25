d06d8a918530e0123d4e82d371e0bdf8c6336f50
namestesensei <christopherwright435@gmail.com>
2024-05-25 07:17:13 +0000

    9.3

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,5 @@
 #include "main.h"
 
-/**
- * times_table - Prints the 9 times table, starting with 0.
- *
- * Description: This function uses two nested loops to print
- * the 9 times table. Each number is separated by a comma and
- * a space, and each line ends with a newline character.
- */
 void times_table(void)
 {
     int i, j;
@@ -24,18 +17,24 @@ void times_table(void)
                 _putchar(' ');
             }
 
-            /* Print extra space for single digit numbers */
-            if (i * j < 10 && j != 0)
-            {
-                _putchar(' ');
-            }
-
             /* Print the number */
-            _putchar(i * j / 10 + '0');
-            _putchar(i * j % 10 + '0');
+            if (i * j < 10)
+            {
+                if (j != 0)
+                {
+                    _putchar(' ');
+                }
+                _putchar(i * j + '0');
+            }
+            else
+            {
+                _putchar(i * j / 10 + '0');
+                _putchar(i * j % 10 + '0');
+            }
         }
 
         /* Print newline at the end of each row */
         _putchar('\n');
     }
 }
+
