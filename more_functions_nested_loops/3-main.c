0713d5c984ed79027189974ae5bf570936f58cb1
namestesensei <christopherwright435@gmail.com>
2024-05-27 05:31:59 -0500

    3.8

---
+++ more_functions_nested_loops/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a = 98, b = 1024;
+    printf("Result of multiplication of %d and %d is %d\n", a, b, mul(a, b));
+    return (0);
+}
+
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function to print a character */
+int _putchar(char c);
+
+/* Function to check if a character is uppercase */
+int _isupper(int c);
+
+/* Function to check if a character is a digit */
+int _isdigit(int c);
+
+/* Function to multiply two integers */
+int mul(int a, int b);
+
+/* Function to print numbers from 0 to 9 */
+void print_numbers(void);
+
+/* Function to print numbers from 0 to 9, excluding 2 and 4 */
+void print_most_numbers(void);
+
+/* Function to print 10 times the numbers, from 0 to 14 */
+void more_numbers(void);
+
+/* Function to draw a straight line in the terminal */
+void print_line(int n);
+
+/* Function to draw a diagonal line on the terminal */
+void print_diagonal(int n);
+
+#endif /* MAIN_H */
+
