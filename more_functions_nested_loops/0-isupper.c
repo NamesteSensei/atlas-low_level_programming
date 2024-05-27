ed1e1b14d379696072ed5d4c454cdea82695d6e5
root <christopherwright435@gmail.com>
2024-05-27 00:43:48 +0000

    0.1

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,7 +4,8 @@
  *  _isupper - checks for uppercase character
  *  @c: the character to check
  *
- *  Return: 1 if c is uppercase, 0otherwise*/
+ *  Return: 1 if c is uppercase, 0 otherwise
+ *  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
