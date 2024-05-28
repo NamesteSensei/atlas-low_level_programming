460107e13f7c07c5ce36fae89cf8b45a21115bd4
root <namestesensei>
2024-05-28 04:13:09 +0000

    5.0

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ * Return: void
+ */
+void rev_string(char *s)
+{
+    int length = 0;
+    int i;
+    char temp;
+
+    /* get the length of the string */
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+
+    /* swap the characters of the string */
+    for (i = 0; i < length / 2; i++)
+    {
+        temp = s[i];
+        s[i] = s[length - i - 1];
+        s[length - i - 1] = temp;
+    }
+}
+
