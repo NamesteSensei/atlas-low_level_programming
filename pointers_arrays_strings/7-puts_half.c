b394d39dc0b2995d27a1bc2c3caff67899c8f481
root <namestesensei>
2024-05-28 04:40:59 +0000

    7.0

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,6 +9,7 @@ void puts_half(char *str)
 {
     int length = 0;
     int i;
+    int start;
 
     /* get the length of the string */
     while (str[length] != '\0')
@@ -17,7 +18,7 @@ void puts_half(char *str)
     }
 
     /* calculate the start index of the second half */
-    int start = length % 2 == 0 ? length / 2 : (length - 1) / 2 + 1;
+    start = length % 2 == 0 ? length / 2 : (length - 1) / 2 + 1;
 
     /* print the second half of the string */
     for (i = start; i < length; i++)
