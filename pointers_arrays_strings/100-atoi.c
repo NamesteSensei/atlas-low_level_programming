64d3fe83bfd18db13d2c610bd3b8eff9c6c19d0f
root <namestesensei>
2024-05-28 03:47:03 +0000

    100.1

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+    int i = 0, num = 0, sign = 1;
+
+    /* handle signs */
+    while (s[i])
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+            continue;
+        else if (s[i] >= '0' && s[i] <= '9')
+            break;
+        i++;
+    }
+
+    /* convert string to integer */
+    while (s[i] && (s[i] >= '0' && s[i] <= '9'))
+    {
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return (num * sign);
+}
+
