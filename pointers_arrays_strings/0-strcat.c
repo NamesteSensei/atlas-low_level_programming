98707efcd8758238dd40921f3a90ad55c9698072
namestesensei <christopherwright435@gmail.com>
2024-06-02 23:16:07 +0000

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i, j;
+	int i, j;
 
-    /* Find the end of the dest string */
-    for (i = 0; dest[i] != '\0'; i++)
-        ;
+	/* Find the end of the dest string */
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-    /* Copy the src string to the end of the dest string */
-    for (j = 0; src[j] != '\0'; j++)
-        dest[i+j] = src[j];
+	/* Copy the src string to the end of the dest string */
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
 
-    /* Add the null byte at the end */
-    dest[i+j] = '\0';
+	/* Add the null byte at the end */
+	dest[i + j] = '\0';
 
-    /* Return the concatenated string */
-    return dest;
+	/* Return the concatenated string */
+	return (dest);
 }
 
