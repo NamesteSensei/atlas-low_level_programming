61b1c93f48a16578f23da075ac0863e7e7d8170d
namestesensei <user.email>
2024-06-03 22:18:14 +0000

    main.h

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,19 +1,35 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+    unsigned int i;
+
+    i = 0;
+    while (i < size)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", buffer[i]);
+        i++;
+    }
+    printf("\n");
+}
+
 int main(void)
 {
-    int n;
+    char buffer[98] = {0x00};
 
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);    
     return (0);
 }
 
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -30,3 +30,5 @@ void print_array(int *a, int n);
 
 /* Function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest */
 char *_strcpy(char *dest, char *
+
+#endif 
