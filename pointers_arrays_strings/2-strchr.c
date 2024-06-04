4cc41d90d7fd2a6d63ec12d3cb75babc2110ba5a
namestesensei <user.email>
2024-06-04 01:52:31 +0000

    2.0

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,12 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-    do {
-        if (*s == c)
-        {
-            return (s);
-        }
-    } while (*s++);
-    return (0);
+	do {
+		if (*s == c)
+		{
+			return (s);
+		}
+	} while (*s++);
+	return (0);
 }
 
