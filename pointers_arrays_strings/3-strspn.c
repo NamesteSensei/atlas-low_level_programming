a5053e7e871367107636912dadb17fd1992d5e01
namestesensei <user.email>
2024-06-04 02:02:38 +0000

    3.0

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,29 +10,29 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int bytes = 0;
-    int flag;
-    char *start = accept;
+	unsigned int bytes = 0;
+	int flag;
+	char *start = accept;
 
-    while (*s)
-    {
-        flag = 0;
-        while (*accept)
-        {
-            if (*s == *accept)
-            {
-                bytes++;
-                flag = 1;
-            }
-            accept++;
-        }
-        if (flag == 0)
-        {
-            return (bytes);
-        }
-        s++;
-        accept = start;
-    }
-    return (bytes);
+	while (*s)
+	{
+		flag = 0;
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				bytes++;
+				flag = 1;
+			}
+			accept++;
+		}
+		if (flag == 0)
+		{
+			return (bytes);
+		}
+		s++;
+		accept = start;
+	}
+	return (bytes);
 }
 
