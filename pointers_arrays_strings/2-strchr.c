e06bda1dee8f44a204ad1c6f3ae7914e0dd91e1f
namestesensei <user.email>
2024-06-04 01:55:24 +0000

    2.1

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,7 @@
  * @s: the string to be searched
  * @c: the character to be located
  *
- * Return: a pointer to the first occurrence of the character c in the string s,
+ * Return: pointer to the first occurrence of the character c in the string s,
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
