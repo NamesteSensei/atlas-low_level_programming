cfbcdd5be6cade8e1496d122bd2e7a4294b149dc
namestesensei <user.email>
2024-06-04 02:00:18 +0000

    3.0

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,13 +1,19 @@
+#include "main.h"
 #include <stdio.h>
 
-// Assuming the function prototype is available in a header file
-// or you can declare it here if it's not in a header file.
-size_t my_strspn(const char *str1, const char *str2);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-int main() {
-    char str1[30] = "1234567890";
-    char str2[30] = "123";
-    printf("Length of initial segment matching str2 in str1: %zu\n", my_strspn(str1, str2));
-    return 0;
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
 
--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,38 @@
-#include <stdio.h>
+#include "main.h"
 
-size_t my_strspn(const char *str1, const char *str2) {
-    const char *p = str1;
-    while(*p) {
-        const char *p2 = str2;
-        while(*p2) {
-            if(*p == *p2) {
-                break;
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to be searched
+ * @accept: the prefix to be measured
+ *
+ * Return: the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int bytes = 0;
+    int flag;
+    char *start = accept;
+
+    while (*s)
+    {
+        flag = 0;
+        while (*accept)
+        {
+            if (*s == *accept)
+            {
+                bytes++;
+                flag = 1;
             }
-            p2++;
+            accept++;
         }
-        if(*p2 == '\0') {
-            break;
+        if (flag == 0)
+        {
+            return (bytes);
         }
-        p++;
+        s++;
+        accept = start;
     }
-    return p - str1;
-}
-
-int main() {
-    char str1[30] = "1234567890";
-    char str2[30] = "123";
-    printf("Length of initial segment matching str2 in str1: %zu\n", my_strspn(str1, str2));
-    return 0;
+    return (bytes);
 }
 
