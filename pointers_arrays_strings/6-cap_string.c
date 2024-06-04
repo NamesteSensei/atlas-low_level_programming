a51ce9f243d68e106357ef64407d8709fc0269ae
namestesensei <user.email>
2024-06-04 03:17:38 +0000

    6.0

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,33 +9,33 @@
  */
 char *cap_string(char *s)
 {
-    int i = 0;
-    int j;
-    char separators[] = " \t\n,;.!?\"(){}";
+	int i = 0;
+	int j;
+	char separators[] = " \t\n,;.!?\"(){}";
 
-    /* Capitalize the first character if it is a letter */
-    if (s[i] >= 'a' && s[i] <= 'z')
-    {
-        s[i] = s[i] - 'a' + 'A';
-    }
+	/* Capitalize the first character if it is a letter */
+	if (s[i] >= 'a' && s[i] <= 'z')
+	{
+		s[i] = s[i] - 'a' + 'A';
+	}
 
-    /* Go through the string, looking for word separators */
-    for (i = 1; s[i] != '\0'; i++)
-    {
-        /* Check if the character is a separator */
-        for (j = 0; separators[j] != '\0'; j++)
-        {
-            if (s[i - 1] == separators[j])
-            {
-                /* If the next character is a lowercase letter, capitalize it */
-                if (s[i] >= 'a' && s[i] <= 'z')
-                {
-                    s[i] = s[i] - 'a' + 'A';
-                }
-            }
-        }
-    }
+	/* Go through the string, looking for word separators */
+	for (i = 1; s[i] != '\0'; i++)
+	{
+		/* Check if the character is a separator */
+		for (j = 0; separators[j] != '\0'; j++)
+		{
+			if (s[i - 1] == separators[j])
+			{
+				/* If the next character is a lowercase letter, capitalize it */
+				if (s[i] >= 'a' && s[i] <= 'z')
+				{
+					s[i] = s[i] - 'a' + 'A';
+				}
+			}
+		}
+	}
 
-    return (s);
+	return (s);
 }
 
