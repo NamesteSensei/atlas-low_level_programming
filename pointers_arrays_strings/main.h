acfe8c71e915564394bdc0e28ab197e7d171987a
namestesensei <user.email>
2024-06-04 01:45:20 +0000

    main.flo

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,38 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function that takes a pointer to an int as parameter and updates the value it points to to 98 */
-void reset_to_98(int *n);
+/* Function that locates a character in a string */
+char *_strchr(char *s, char c);
 
-/* Function that swaps the values of two integers */
-void swap_int(int *a, int *b);
+/* Function that gets the length of a prefix substring */
+unsigned int _strspn(char *s, char *accept);
 
-/* Function that returns the length of a string */
-int _strlen(char *s);
+/* Function that searches a string for any of a set of bytes */
+char *_strpbrk(char *s, char *accept);
 
-/* Function that prints a string, followed by a new line, to stdout */
-void _puts(char *str);
+/* Function that locates a substring */
+char *_strstr(char *haystack, char *needle);
 
-/* Function that prints a string, in reverse, followed by a new line */
-void print_rev(char *s);
+/* Function that prints the chessboard */
+void print_chessboard(char (*a)[8]);
 
-/* Function that reverses a string */
-void rev_string(char *s);
-
-/* Function that prints every other character of a string, starting with the first character, followed by a new line */
-void puts2(char *str);
-
-/* Function that prints half of a string, followed by a new line */
-void puts_half(char *str);
-
-/* Function that prints n elements of an array of integers, followed by a new line */
-void print_array(int *a, int n);
-
-/* Function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest */
-char *_strcpy(char *dest, char *src);
-
-/* Function that copies n bytes from memory area src to memory area dest */
-char *_memcpy(char *dest, char *src, unsigned int n);
+/* Function that prints the sum of the two diagonals of a square matrix of integers */
+void print_diagsums(int *a, int size);
 
 #endif /* MAIN_H */
 
