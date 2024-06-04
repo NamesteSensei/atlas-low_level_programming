0ba2b947749c9890ba61c529308c95ccf0db7240
namestesensei <user.email>
2024-06-04 03:01:05 +0000

    main.hMPA

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,8 +19,26 @@ char *_strstr(char *haystack, char *needle);
 /* Function that prints the chessboard */
 void print_chessboard(char (*a)[8]);
 
-/* Function that prints the sum of the two diagonals of a square matrix of integers */
-void print_diagsums(int *a, int size);
+/* Function that concatenates two strings */
+char *_strcat(char *dest, char *src);
+
+/* Function that concatenates two strings using at most n bytes from src */
+char *_strncat(char *dest, char *src, int n);
+
+/* Function that copies a string */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Function that compares two strings */
+int _strcmp(char *s1, char *s2);
+
+/* Function that reverses the content of an array of integers */
+void reverse_array(int *a, int n);
+
+/* Function that changes all lowercase letters of a string to uppercase */
+char *string_toupper(char *);
+
+/* Function that capitalizes all words of a string */
+char *cap_string(char *);
 
 #endif /* MAIN_H */
 
