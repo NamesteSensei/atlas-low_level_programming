fd38b87cd8fa431bc9f85f0e9f81981ebdbc9c32
namestesensei <user.email>
2024-06-04 02:23:02 +0000

    5.0

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,26 +11,26 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i;
+	int i;
 
-    if (*needle == 0)
-        return (haystack);
+	if (*needle == 0)
+		return (haystack);
 
-    while (*haystack)
-    {
-        i = 0;
+	while (*haystack)
+	{
+		i = 0;
 
-        if (haystack[i] == needle[i])
-        {
-            do {
-                if (needle[i + 1] == '\0')
-                    return (haystack);
-                i++;
-            } while (haystack[i] == needle[i]);
-        }
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
+		}
 
-        haystack++;
-    }
-    return ('\0');
+		haystack++;
+	}
+	return ('\0');
 }
 
