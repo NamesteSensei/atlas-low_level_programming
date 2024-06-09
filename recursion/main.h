a5a8eb8b34c5ec3cbc6ea9c1130d4c3d59c8ff95
namestesensei <christopherwright435@gmail.com>
2024-06-09 06:16:27 +0000

    0-puts_recursion.c

---
+++ recursion/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Prints a string, followed by a new line */
+void _puts_recursion(char *s);
+
+/* Prints a string in reverse */
+void _print_rev_recrsion(char *s);
+
+/* Returnsa the length of a string
+ */
+int _strlen_recursion(char *s);
+
+/* Returns the value of x raised to the power of y */
+int _pow_recursion(int x, int y);
+
+/* Returns the natural square root of a number */
+int _sqrt_recursion(int n);
+
+/* Returns 1 if the input integer is a prime number, otherwise return 0 */
+int is_prime_number(int n);
+
+#endif /* MAIN_H */
