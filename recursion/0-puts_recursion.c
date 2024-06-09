2489bf136ad98df6b7e5fa0a82d81f7a63f82dd3
namestesensei <christopherwright435@gmail.com>
2024-06-09 07:11:00 +0000

    0.1

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- *  _puts_recursion - prints a stirng, follwed by a new line 
- *  @s: string to print
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
  *
- *  Return: void
+ * Return: void
  */
-
 void _puts_recursion(char *s)
 {
-	if(*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+    if (*s) {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    } else {
+        _putchar('\n');
+    }
 }
+
