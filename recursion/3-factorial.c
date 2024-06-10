88e8da5a82cb7c70ed26e57c1ceb9371e0c73add
namestesensei <christopherwright435@gmail.com>
2024-06-10 00:21:05 +0000

    3.5

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,22 +2,23 @@
 
 /**
  * factorial - calculates the factorial of a given number
- * @n: the number to calculate the factorial of 
+ * @n: the number to calculate the factorial of
  *
  * Return: the factorial of n, or -1 if n is less than 0
  */
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		retrun (-1); /* Error for negative numbners */
-	}
-	else if (n == 0)
-	{
-		return (1); /* Base case: 0! = 1 */
-	}
-	else
-	{
-		return (n * factorial(n -1)); /* Recursive case */
-	}
+    if (n < 0)
+    {
+        return (-1); /* Error for negative numbers */
+    }
+    else if (n == 0)
+    {
+        return (1); /* Base case: 0! = 1 */
+    }
+    else
+    {
+        return (n * factorial(n - 1)); /* Recursive case */
+    }
 }
+
