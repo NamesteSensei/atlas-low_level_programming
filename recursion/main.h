26535bbed9c74c8eda9ead2a4e64e54860eb1ed0
namestesensei <christopherwright435@gmail.com>
2024-06-10 01:19:17 +0000

    main.h.0

--- recursion/main.h
+++ recursion/main.h
@@ -1,26 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prints a character */
 int _putchar(char c);
-
-/* Prints a string, followed by a new line */
-void _puts_recursion(char *s);
-
-/* Prints a string in reverse */
-void _print_rev_recursion(char *s);  /* Corrected function name */
-
-/* Returns the length of a string */
-int _strlen_recursion(char *s);
-
-/* Returns the value of x raised to the power of y */
-int _pow_recursion(int x, int y);
-
-/* Returns the natural square root of a number */
-int _sqrt_recursion(int n);
-
-/* Returns 1 if the input integer is a prime number, otherwise return 0 */
-int is_prime_number(int n);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
 
