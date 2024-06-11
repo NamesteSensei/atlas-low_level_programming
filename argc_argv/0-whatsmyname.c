51539cc321721e7950d3f847e5287c9233b07bf8
namestesensei <christopherwright435@gmail.com>
2024-06-11 03:24:01 +0000

    2.0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints the name of the program
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
+ *
+ * Return: 0 if successful
+ */
 int main(int argc, char *argv[])
 {
 	if (argc >= 1)
