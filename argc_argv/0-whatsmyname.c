3ba3f86e82c07c716004a58ed6da8ca1e576d227
namestesensei <christopherwright435@gmail.com>
2024-06-11 03:21:34 +0000

    ThisShouldDo1

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc >= 1)
-    {
-        printf("%s\n", argv[0]);
-    }
-    return (0);
+	if (argc >= 1)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
 }
 
