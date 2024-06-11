1dd1a634a79c34829d90fca779f98adac4c6c7ac
namestesensei <christopherwright435@gmail.com>
2024-06-11 04:23:37 +0000

    5.oh

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,14 +10,14 @@ int sqrt_helper(int n, int i);
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (sqrt_helper(n, 0));
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, 0));
+	}
 }
 
 /**
@@ -28,17 +28,17 @@ int _sqrt_recursion(int n)
  */
 int sqrt_helper(int n, int i)
 {
-    if (i * i > n)
-    {
-        return (-1);
-    }
-    else if (i * i == n)
-    {
-        return (i);
-    }
-    else
-    {
-        return (sqrt_helper(n, i + 1));
-    }
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	else
+	{
+		return (sqrt_helper(n, i + 1));
+	}
 }
 
