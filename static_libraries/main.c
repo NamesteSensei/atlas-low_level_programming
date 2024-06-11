41bf0a98a793d2f712f23bc821d2263f82968790
namestesensei <christopherwright435@gmail.com>
2024-06-11 01:55:52 +0000

    READ

--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,155 +1,88 @@
 #include "main.h"
-#include <unistd.h>
 
-/* _putchar function */
-int _putchar(char c)
+int main(void)
 {
-    return write(1, &c, 1);
-}
+    char s[] = "Hello, World!";
+    char t[] = "Goodbye, World!";
+    char u[50];
+    char v[] = "World";
+    char *w;
 
-/* _islower function */
-int _islower(int c)
-{
-    return (c >= 'a' && c <= 'z');
-}
-
-/* _isalpha function */
-int _isalpha(int c)
-{
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-/* _abs function */
-int _abs(int n)
-{
-    return (n < 0 ? -n : n);
-}
-
-/* _isupper function */
-int _isupper(int c)
-{
-    return (c >= 'A' && c <= 'Z');
-}
-
-/* _isdigit function */
-int _isdigit(int c)
-{
-    return (c >= '0' && c <= '9');
-}
-
-/* _strlen function */
-int _strlen(char *s)
-{
-    int length = 0;
-
-    while (*s++)
-    {
-        length++;
-    }
-
-    return (length);
-}
-
-/* _puts function */
-void _puts(char *s)
-{
-    while (*s)
-    {
-        _putchar(*s++);
-    }
+    /* Test _putchar */
+    _putchar('H');
     _putchar('\n');
-}
 
-/* _strcpy function */
-char *_strcpy(char *dest, char *src)
-{
-    char *ret = dest;
+    /* Test _islower */
+    printf("%d\n", _islower('a')); /* should print 1 */
+    printf("%d\n", _islower('A')); /* should print 0 */
 
-    while (*src)
+    /* Test _isalpha */
+    printf("%d\n", _isalpha('a')); /* should print 1 */
+    printf("%d\n", _isalpha('1')); /* should print 0 */
+
+    /* Test _abs */
+    printf("%d\n", _abs(-5)); /* should print 5 */
+    printf("%d\n", _abs(5));  /* should print 5 */
+
+    /* Test _isupper */
+    printf("%d\n", _isupper('a')); /* should print 0 */
+    printf("%d\n", _isupper('A')); /* should print 1 */
+
+    /* Test _isdigit */
+    printf("%d\n", _isdigit('a')); /* should print 0 */
+    printf("%d\n", _isdigit('1')); /* should print 1 */
+
+    /* Test _strlen */
+    printf("%d\n", _strlen(s)); /* should print 13 */
+
+    /* Test _puts */
+    _puts(s); /* should print "Hello, World!" */
+
+    /* Test _strcpy */
+    printf("%s\n", _strcpy(u, s)); /* should print "Hello, World!" */
+
+    /* Test _atoi */
+    printf("%d\n", _atoi("98")); /* should print 98 */
+    printf("%d\n", _atoi("-98")); /* should print -98 */
+
+    /* Test _strcat */
+    printf("%s\n", _strcat(s, t)); /* should print "Hello, World!Goodbye, World!" */
+
+    /* Test _strncat */
+    printf("%s\n", _strncat(s, t, 5)); /* should print "Hello, World!Goodbye, World!Goodb" */
+
+    /* Test _strncpy */
+    printf("%s\n", _strncpy(u, s, 5)); /* should print "Hello" */
+
+    /* Test _strcmp */
+    printf("%d\n", _strcmp(s, t)); /* should print a positive number */
+
+    /* Test _memset */
+    printf("%s\n", _memset(u, 'a', 5)); /* should print "aaaaa" */
+
+    /* Test _memcpy */
+    printf("%s\n", _memcpy(u, s, 5)); /* should print "Hello" */
+
+    /* Test _strchr */
+    printf("%s\n", _strchr(s, 'o')); /* should print "o, World!Goodbye, World!Goodb" */
+
+    /* Test _strspn */
+    printf("%u\n", _strspn(s, "Helo")); /* should print 5 */
+
+    /* Test _strpbrk */
+    printf("%s\n", _strpbrk(s, "xyz")); /* should print "(null)" */
+
+    /* Test _strstr */
+    w = _strstr(s, v);
+    if (w)
     {
-        *dest++ = *src++;
+        printf("%s\n", w); /* should print "World!Goodbye, World!Goodb" */
     }
-    *dest = '\0';
-
-    return (ret);
-}
-
-/* _atoi function */
-int _atoi(char *s)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strcat function */
-char *_strcat(char *dest, char *src)
-{
-    char *ret = dest;
-
-    while (*dest)
+    else
     {
-        dest++;
+        printf("(null)\n");
     }
 
-    while (*src)
-    {
-        *dest++ = *src++;
-    }
-    *dest = '\0';
-
-    return (ret);
-}
-
-/* _strncat function */
-char *_strncat(char *dest, char *src, int n)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strncpy function */
-char *_strncpy(char *dest, char *src, int n)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strcmp function */
-int _strcmp(char *s1, char *s2)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _memset function */
-char *_memset(char *s, char b, unsigned int n)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _memcpy function */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strchr function */
-char *_strchr(char *s, char c)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strspn function */
-unsigned int _strspn(char *s, char *accept)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strpbrk function */
-char *_strpbrk(char *s, char *accept)
-{
-    /* This function is complex and requires more code */
-}
-
-/* _strstr function */
-char *_strstr(char *haystack, char *needle)
-{
-    /* This function is complex and requires more code */
+    return (0);
 }
 
