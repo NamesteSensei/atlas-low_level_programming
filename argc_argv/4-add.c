1259d5d2916fcac4e14c4618d5d51a92af4a13d6
namestesensei <christopherwright435@gmail.com>
2024-06-11 03:46:39 +0000

    3.2

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,17 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum = 0;
+	int i, sum = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		if (!atoi(argv[i]))
 		{
-			if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
 		sum += atoi(argv[i]);
 	}
