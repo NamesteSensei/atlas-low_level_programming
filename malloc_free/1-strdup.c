2b92319122bdf0c198e19319d38b1da16e5d2a80
namestesensei <christopherwright435@gmail.com>
2024-06-13 15:10:36 +0000

    1.1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,25 +11,25 @@
  */
 char *_strdup(char *str)
 {
-    char *duplicate;
-    int index, len = 0;
+	char *duplicate;
+	int index, len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    for (len = 0; str[len]; len++)
-        ;
+	for (len = 0; str[len]; len++)
+		;
 
-    duplicate = malloc(sizeof(char) * (len + 1));
+	duplicate = malloc(sizeof(char) * (len + 1));
 
-    if (duplicate == NULL)
-        return (NULL);
+	if (duplicate == NULL)
+		return (NULL);
 
-    for (index = 0; str[index]; index++)
-        duplicate[index] = str[index];
+	for (index = 0; str[index]; index++)
+		duplicate[index] = str[index];
 
-    duplicate[len] = '\0';
+	duplicate[len] = '\0';
 
-    return (duplicate);
+	return (duplicate);
 }
 
