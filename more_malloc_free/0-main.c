e0aca63e8dc0f49287bb330637c1a00daf20937e
namestesensei <christopherwright435@gmail.com>
2024-06-16 22:25:47 +0000

    main.c0.2

--- more_malloc_free/0-main.c
+++ more_malloc_free/0-main.c
@@ -4,36 +4,29 @@
 #include <limits.h>
 
 /**
- * main - chck the code 
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    // Declare pointers for different data types
     char *c;
     int *i;
     float *f;
     double *d;
 
-    // Allocate memory and print the memory address
     c = malloc_checked(sizeof(char) * 1024);
-    printf("Address of char array: %p\n", (void *)c);
-
+    printf("%p\n", (void *)c);
     i = malloc_checked(sizeof(int) * 402);
-    printf("Address of int array: %p\n", (void *)i);
-
+    printf("%p\n", (void *)i);
     f = malloc_checked(sizeof(float) * 100000000);
-    printf("Address of float array: %p\n", (void *)f);
-
+    printf("%p\n", (void *)f);
     d = malloc_checked(INT_MAX);
-    printf("Address of double array: %p\n", (void *)d);
-
-    // Free the allocated memory
+    printf("%p\n", (void *)d);
     free(c);
     free(i);
     free(f);
     free(d);
-
     return (0);
 }
+
