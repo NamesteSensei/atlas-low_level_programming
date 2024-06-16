4490cbcb57936768790220f173addc4a6d3f2f03
namestesensei <christopherwright435@gmail.com>
2024-06-16 23:39:39 +0000

    main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,6 +5,5 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 int *array_range(int min, int max);
-void simple_print_buffer(int *buffer, unsigned int size);
 
 #endif /* MAIN_H */
