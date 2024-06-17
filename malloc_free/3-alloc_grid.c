4aefae6120cc60959b65e1ee663e57a81da7fe06
namestesensei <christopherwright435@gmail.com>
2024-06-17 03:28:55 +0000

    3.glow

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,34 +11,34 @@
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid, index, sub_index;
+int **grid, index, sub_index;
 
-    if (width <= 0 || height <= 0)
-        return (NULL);
+if (width <= 0 || height <= 0)
+return (NULL);
 
-    grid = malloc(sizeof(int *) * height);
+grid = malloc(sizeof(int *) * height);
 
-    if (grid == NULL)
-        return (NULL);
+if (grid == NULL)
+return (NULL);
 
-    for (index = 0; index < height; index++)
-    {
-        grid[index] = malloc(sizeof(int) * width);
-        if (grid[index] == NULL)
-        {
-            for (; index >= 0; index--)
-                free(grid[index]);
-            free(grid);
-            return (NULL);
-        }
-    }
+for (index = 0; index < height; index++)
+{
+grid[index] = malloc(sizeof(int) * width);
+if (grid[index] == NULL)
+{
+for (; index >= 0; index--)
+free(grid[index]);
+free(grid);
+return (NULL);
+}
+}
 
-    for (index = 0; index < height; index++)
-    {
-        for (sub_index = 0; sub_index < width; sub_index++)
-            grid[index][sub_index] = 0;
-    }
+for (index = 0; index < height; index++)
+{
+for (sub_index = 0; sub_index < width; sub_index++)
+grid[index][sub_index] = 0;
+}
 
-    return (grid);
+return (grid);
 }
 
