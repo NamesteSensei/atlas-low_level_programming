4fdc8d329ec8b9e5198f8970f0489901788ac169
namestesensei <christopherwright435@gmail.com>
2024-06-17 02:17:52 +0000

    2.0

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,1 +1,13 @@
-sdfasf
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from
+ * 
+ * Return: Always 0.
+ */
+int main(void)
+{
+    printf("%s\n", __FILE__);
+    return (0);
+}
+
