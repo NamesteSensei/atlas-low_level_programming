bb5a986528841f8113f4b78216e20ca5c4f465b3
namestesensei <christopherwright435@gmail.com>
2024-06-20 10:14:23 +0000

    doghouse

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,13 +2,10 @@
 #define DOG_H
 
 /**
- * struct dog - A structure representing a dog.
- * @name: A pointer to a character string representing the dog's name.
- * @age: A float representing the dog's age.
- * @owner: A pointer to a character string representing the dog's owner.
- *
- * Description: This structure stores information about a dog, including
- *              its name, age, and owner's name.
+ * struct dog - a structure defining a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
  */
 struct dog
 {
