9bc51ac11754f9484b88d8c2998b4bab28ad3555
namestesensei <christopherwright435@gmail.com>
2024-06-25 03:20:01 +0000

    func

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,16 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stdio.h>  /* Include necessary headers if required */
+#include <stdio.h> 
 
-/* Function prototype for print_name_as_is */
-void print_name_as_is(char *name);
-
-/* Function prototype for print_name_uppercase */
-void print_name_uppercase(char *name);
-
-/* Function prototype for print_name */
-void print_name(char *name, void (*f)(char *));
+/* Function prototypes */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
-
+i
