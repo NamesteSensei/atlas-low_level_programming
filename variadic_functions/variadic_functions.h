e07b69ddac169bfd436b1c49d94f9de93cb6eda1
namestesensei <christopherwright435@gmail.com>
2024-06-25 04:47:42 +0000

    2.0

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
