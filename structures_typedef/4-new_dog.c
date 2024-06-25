308ec5182d1c128e3def06ca69cacda3f71c1e26
namestesensei <christopherwright435@gmail.com>
2024-06-25 00:59:28 +0000

    4.2

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,6 +4,24 @@
 #include "dog.h"
 
 /**
+ * _strdup - Duplicates a string using malloc
+ * @str: The string to duplicate
+ *
+ * Return: Pointer to the duplicated string or NULL if malloc fails
+ */
+char *_strdup(char *str)
+{
+    char *dup;
+    size_t len = strlen(str) + 1;
+
+    dup = malloc(len);
+    if (dup == NULL)
+        return NULL;
+
+    return strcpy(dup, str);
+}
+
+/**
  * new_dog - Creates a new dog_t structure
  * @name: The name of the dog
  * @age: The age of the dog
@@ -15,37 +33,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
     dog_t *new_dog;
-    char *name_copy;
-    char *owner_copy;
 
     /* Allocate memory for new dog_t structure */
     new_dog = malloc(sizeof(dog_t));
     if (new_dog == NULL)
         return NULL;
 
-    /* Allocate memory and copy name */
-    name_copy = malloc(strlen(name) + 1);
-    if (name_copy == NULL)
+    /* Duplicate and store name */
+    new_dog->name = _strdup(name);
+    if (new_dog->name == NULL)
     {
         free(new_dog);
         return NULL;
     }
-    strcpy(name_copy, name);
 
-    /* Allocate memory and copy owner */
-    owner_copy = malloc(strlen(owner) + 1);
-    if (owner_copy == NULL)
+    /* Duplicate and store owner */
+    new_dog->owner = _strdup(owner);
+    if (new_dog->owner == NULL)
     {
-        free(name_copy);
+        free(new_dog->name);
         free(new_dog);
         return NULL;
     }
-    strcpy(owner_copy, owner);
 
     /* Assign values to the new dog_t structure */
-    new_dog->name = name_copy;
     new_dog->age = age;
-    new_dog->owner = owner_copy;
 
     return new_dog;
 }
