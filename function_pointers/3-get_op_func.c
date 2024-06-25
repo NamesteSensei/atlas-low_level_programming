2f084de8ebcc3f010722aba9afddb6d7c74ae8b9
namestesensei <christopherwright435@gmail.com>
2024-06-25 04:19:27 +0000

    go

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,15 +1,5 @@
 #include "3-calc.h"
 
-/* Array of structs to define operations */
-op_t ops[] = {
-    {"+", op_add},
-    {"-", op_sub},
-    {"*", op_mul},
-    {"/", op_div},
-    {"%", op_mod},
-    {NULL, NULL}
-};
-
 /* Function to select the correct operation */
 int (*get_op_func(char *s))(int, int)
 {
