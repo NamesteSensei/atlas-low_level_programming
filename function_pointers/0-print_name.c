e0f35470d24f6b5abe61583bd3d37c0a3d3b5a8e
namestesensei <christopherwright435@gmail.com>
2024-06-29 00:18:50 +0000

    fp

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,20 @@
+
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name using a specified printing function.
- * @name: Pointer to a string containing the name to print.
- * @f: Pointer to a function that takes a char * as parameter and returns void.
+ * print_name - prints a name using a given function
+ * @name: name to be printed
+ * @f: pointer to the function that prints the name
  *
- * Description:
- * This function takes a name and a function pointer @f. It calls
- * the function pointed to by @f with @name as argument, thereby printing the name.
+ * Description: This function takes a name and a function pointer,
+ * and calls the function pointer with the name.
  */
 void print_name(char *name, void (*f)(char *))
 {
     if (name != NULL && f != NULL)
+    {
         f(name);
+    }
 }
 
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,7 +1,15 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * print_name - prints a name using a given function
+ * @name: name to be printed
+ * @f: pointer to the function that prints the name
+ *
+ * Description: This function takes a name and a function pointer,
+ * and calls the function pointer with the name.
+ */
+void print_name(char *name, void (*f)(char *));
 
 #endif /* FUNCTION_POINTERS_H */
 
