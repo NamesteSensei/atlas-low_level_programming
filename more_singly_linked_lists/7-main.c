4d5f74a1bd3440d16f54e5ef1eb4b8e1d6eb30e6
root <christopherwright435@gmail.com>
2024-07-07 08:04:47 +0000

    7.0

---
+++ more_singly_linked_lists/7-main.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+	listint_t *node;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+	print_listint(head);
+
+	/* Get the 5th node and print its data */
+
+	node = get_nodeint_at_index(head, 5);
+	if (node != NULL)
+		printf("Node at index 5: %d\n", node->n);
+	else
+		printf("Node at index 5: NULL\n");
+
+	/* Free the list */
+	free_listint2(&head);
+	return (0);
+}
+
