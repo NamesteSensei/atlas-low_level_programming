7ccfe6eff1ddc70863fe38529f94a2df9603b2c9
root <christopherwright435@gmail.com>
2024-07-07 08:19:34 +0000

    9.0

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     listint_t *new_node;
     listint_t *temp = *head;
 
+    if (head == NULL)
+        return (NULL);
+
     new_node = malloc(sizeof(listint_t));
     if (new_node == NULL)
         return (NULL);
@@ -31,8 +34,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
     for (i = 0; temp != NULL && i < idx - 1; i++)
         temp = temp->next;
 
-    if (temp == NULL || temp->next == NULL)
+    if (temp == NULL)
+    {
+        free(new_node);
         return (NULL);
+    }
 
     new_node->next = temp->next;
     temp->next = new_node;
