fa5d6557af4202d0851fd0b72162f67dd7aa9827
namestesensei <christopherwright435@gmail.com>
2024-07-13 00:12:38 +0000

    2.1 code Reload

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,27 +1,28 @@
 #include <stdarg.h>  /* Include for variadic functions */
 #include <stdio.h>   /* Include for printf */
-#include "variadic_functions.h"
+#include "variadic_functions.h"  /* Your custom header, if needed */
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    char *str;
+	va_list args;
+	unsigned int i;
+	char *str;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++) {
-        str = va_arg(args, char *);
-        if (str == NULL)
-            printf("(nil)");
-        else
-            printf("%s", str);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 
-        if (separator != NULL && i != n - 1)
-            printf("%s", separator);
-    }
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
 
-    printf("\n");
-    va_end(args);
+	printf("\n");
+	va_end(args);
 }
 
