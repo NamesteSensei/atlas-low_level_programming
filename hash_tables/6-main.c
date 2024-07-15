13504eef82b9a727d563405a8f06206e7ab5e608
namestesensei <christopherwright435@gmail.com>
2024-07-15 21:56:13 +0000

    6.0

---
+++ hash_tables/6-main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - Check the code for the hash_table_delete function.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+
+	ht = hash_table_create(1024);
+	if (ht == NULL)
+	{
+		fprintf(stderr, "Error: Hash table creation failed\n");
+		return (EXIT_FAILURE);
+	}
+
+	hash_table_set(ht, "c", "fun");
+	hash_table_set(ht, "python", "awesome");
+	hash_table_set(ht, "Bob", "and Kris love asm");
+	hash_table_set(ht, "N", "queens");
+	hash_table_set(ht, "Asterix", "Obelix");
+	hash_table_set(ht, "Betty", "Cool");
+	hash_table_set(ht, "98", "Battery Street");
+
+	hash_table_print(ht);
+
+	hash_table_delete(ht);
+
+	return (EXIT_SUCCESS);
+}
+
